a773b7ac1400a2dd2aa156cefc8699711e449412
Chris <chris.hart.fd@gmail.com>
2015-06-26 15:57:36 +0100

    Upload

---
+++ www/js/config.js
@@ -0,0 +1,6 @@
+angular.module('your_app_name.config', [])
+.constant('WORDPRESS_API_URL', 'http://eightlane.org/api/')
+.constant('WORDPRESS_PUSH_URL', 'http://eightlane.org/push/')
+.constant('GCM_SENDER_ID', '574597432927')
+
+;
